30d14ff158933c65ba8110aa0f274e6e1429547f
Natalia Millan <natalia@easypeasyapp.com>
2020-10-18 20:39:15 +0200

    add tests for getSearch getQuery and searchByEnter

--- src/App.test.js
+++ src/App.test.js
@@ -27,3 +27,93 @@ test('renders search button', () => {
   expect(searchBtn).toBeInTheDocument();
 });
 
+describe('App component', () => {
+
+  describe('methods', () => {
+    let instance
+
+    beforeEach(() => {
+      const wrapper = shallow(<App />);
+      instance = wrapper.instance()
+    })
+
+    it('getQuery', () => {
+      instance.setState = jest.fn()
+
+      const expectedQuery = 'expectedQuery'
+      const mockEvent = { currentTarget: { value: expectedQuery }}
+
+      instance.getQuery(mockEvent)
+
+      expect(instance.setState).toHaveBeenCalledWith({
+        query: expectedQuery
+      })
+    })
+
+    describe('getSearch', () => {
+
+      beforeEach( () => {
+        instance.setState = jest.fn();
+      })
+
+      it('with only one value', () => {
+        const expectedSearch = 'expectedSearch';
+        const mock = {
+          target: {
+            value: expectedSearch
+          }
+        }
+
+        instance.getSearch(mock)
+
+        expect(instance.setState).toHaveBeenCalledWith({
+          searchBy: [expectedSearch]
+        })
+      })
+
+      it('with many values', () => {
+        const expectedSearch = 'expectedSearch';
+        const mock = {
+          target: {
+            value: ','+expectedSearch
+          }
+        }
+
+        instance.getSearch(mock)
+
+        expect(instance.setState).toHaveBeenCalledWith({
+          searchBy: ['', 'expectedSearch']
+        })
+      })
+    })
+
+    describe('searchByEnter', () => {
+
+      beforeEach(() => {
+        instance.fetchQueryData = jest.fn();
+      })
+
+      it('when pressing enter key', () => {
+        const expectedKey = 'Enter';
+        const mock = {
+          key: expectedKey
+        }
+
+        instance.searchByEnter(mock)
+
+        expect(instance.fetchQueryData).toHaveBeenCalled();
+      })
+
+      it('when pressing a key that is not enter', () => {
+        const expectedKey = 'A';
+        const mock = {
+          key: expectedKey
+        }
+
+        instance.searchByEnter(mock)
+
+        expect(instance.fetchQueryData).not.toHaveBeenCalled();
+      })
+    })
+  })
+})
