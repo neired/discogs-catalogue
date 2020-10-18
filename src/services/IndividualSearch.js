e1d3cbfca7f56de5b8dd94bfe80d0c17083c3d22
Natalia Millan <natalia@easypeasyapp.com>
2020-10-18 12:13:16 +0200

    refactor fetches

--- src/services/IndividualSearch.js
+++ src/services/IndividualSearch.js
@@ -19,7 +19,7 @@ export const fetchIndividualData = async (query, search) => {
   const ENDPOINT = `https://api.discogs.com/database/search?type=${search}&q=${query}&per_page=25`;
   try {
     const res = await fetch(ENDPOINT, options);
-    return await res.json();
+    return res.json();
   } catch (error) {
     return console.log('Looks like there was a problem!', error);
   }
@@ -29,7 +29,7 @@ export const fetchCollection = async () => {
   const ENDPOINT = "https://api.discogs.com/users/neired/collection/folders/0/releases";
   try {
     const res = await fetch(ENDPOINT, options);
-    return await res.json();
+    return res.json();
   } catch (error) {
     return console.log('Looks like we coudnt retrieve your collection!', error);
   }
@@ -39,7 +39,7 @@ export const postRelease = async (id) => {
   const ENDPOINT = `https://api.discogs.com/users/neired/collection/folders/1/releases/${id}`;
   try {
     const res = await fetch(ENDPOINT, optionsPost);
-    return await res.json();
+    return res.json();
   } catch (error) {
     return console.log('We couldnt add that item to your collection!', error);
   }
