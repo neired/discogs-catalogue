fdd4eb9a13fc1ba85c319a62b07e5e3cd63d1244
Natalia Millan <natalia@easypeasyapp.com>
2020-10-20 00:23:00 +0200

    generate build for github pages

---
+++ docs/precache-manifest.cd6aeac78ae1b0ce5355a65444dea508.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aba1e543ff7193600ca3d8dccabf768",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4478d80c965c094a48b2",
+    "url": "./static/css/main.37b9af42.chunk.css"
+  },
+  {
+    "revision": "bdc31acb4c3a35c6f779",
+    "url": "./static/js/2.770d6232.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.770d6232.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4478d80c965c094a48b2",
+    "url": "./static/js/main.b3df6d0a.chunk.js"
+  },
+  {
+    "revision": "930a01989d32b33eacb8",
+    "url": "./static/js/runtime-main.f8cce24d.js"
+  }
+]);
